b875865b00297a0377b634024eec8927931b5d88
newMiodek <130090925+newMiodek@users.noreply.github.com>
2023-06-15 09:58:18 +0200

    Added axes to the graphing area
    Also, dataTable is a table of doubles now.

--- GraphGram/GraphingArea.cs
+++ GraphGram/GraphingArea.cs
@@ -2,19 +2,19 @@
 
 public class GraphingArea : IDrawable {
 
-    private float[,] dataTable;
+    private double[,] dataTable;
     private bool isInputValid = true;
 
     public void PassDataTable(Entry[,] entryTable) {
         // Parse the text from entryTable to represent it as floats
-        float[,] parsedDataTable = new float[entryTable.GetLength(0), 4];
+        double[,] parsedDataTable = new double[entryTable.GetLength(0), 4];
         bool parseSucceded = true;
         for (int i = 0; i < parsedDataTable.GetLength(0); i++) {
             for (int j = 0; j < 4; j++) {
                 // The deal with these locals is the same as in MainPage's constructor
                 int localJ = 1 * j;
                 int localI = 1 * i;
-                if (!float.TryParse(entryTable[localI, localJ].Text, out parsedDataTable[localI, localJ])) {
+                if (!double.TryParse(entryTable[localI, localJ].Text, out parsedDataTable[localI, localJ])) {
                     parseSucceded = false;
                     break;
                 }
@@ -28,10 +28,27 @@ public class GraphingArea : IDrawable {
     }
 
     public void Draw(ICanvas canvas, RectF dirtyRect) {
+        canvas.StrokeColor = Colors.White;
+        // <x axis>
+        // main line
+        canvas.DrawLine(dirtyRect.Left, dirtyRect.Bottom * 0.9f, dirtyRect.Right, dirtyRect.Bottom * 0.9f);
+        // arrow point
+        canvas.DrawLine(dirtyRect.Right, dirtyRect.Bottom * 0.9f, dirtyRect.Right - 10, dirtyRect.Bottom * 0.9f - 10);
+        canvas.DrawLine(dirtyRect.Right, dirtyRect.Bottom * 0.9f, dirtyRect.Right - 10, dirtyRect.Bottom * 0.9f + 10);
+        // </x axis>
+
+        // <y axis>
+        // main line
+        canvas.DrawLine(dirtyRect.Right * 0.1f, dirtyRect.Bottom, dirtyRect.Right * 0.1f, dirtyRect.Top);
+        // arrow point
+        canvas.DrawLine(dirtyRect.Right * 0.1f, dirtyRect.Top, dirtyRect.Right * 0.1f - 10, dirtyRect.Top + 10);
+        canvas.DrawLine(dirtyRect.Right * 0.1f, dirtyRect.Top, dirtyRect.Right * 0.1f + 10, dirtyRect.Top + 10);
+        // </y axis>
+
         if (dataTable != null) {
             canvas.StrokeColor = Colors.Red;
             canvas.StrokeSize = 6;
-            canvas.DrawLine(dataTable[0, 0], dataTable[0, 1], dataTable[1, 0], dataTable[1, 1]);
+            canvas.DrawLine((float)dataTable[0, 0], (float)dataTable[0, 1], (float)dataTable[1, 0], (float)dataTable[1, 1]);
         }
         if (!isInputValid) {
             canvas.FontColor = Colors.Red;
