96877eaa79475bf0016d515b13bc1b7bd6fa449d
newMiodek <130090925+newMiodek@users.noreply.github.com>
2023-07-13 16:42:44 +0200

    Expanded the About tab

--- GraphGram/AboutPage.xaml.cs
+++ GraphGram/AboutPage.xaml.cs
@@ -4,4 +4,14 @@ public partial class AboutPage : ContentPage {
     public AboutPage() {
 		InitializeComponent();
 	}
+
+	private async void LicenseButtonClick(object sender, EventArgs e) {
+		Button button = (Button)sender;
+		if(button == CommunityToolkitMvvmButton)
+			await Launcher.OpenAsync("https://github.com/CommunityToolkit/dotnet/blob/main/License.md");
+		else if(button == InkscapeButton)
+            await Launcher.OpenAsync("https://inkscape.org/about/license/");
+		else if(button == MauiButton)
+			await Launcher.OpenAsync("https://github.com/dotnet/maui/blob/main/LICENSE.txt");
+    }
 }
